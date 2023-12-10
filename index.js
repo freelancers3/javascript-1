e7c547264aaebe921d1e48f42306f70bc9d693d5
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-12-10 19:59:24 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// alert("Hello World !!")
+//console.log("Hello World !!");
+
+var x = 5;
+var name = "Safwan";
+
+let c = 6;
+const B = 15;
+//B = 10;  // TypeError: Assignment to constant variable.
+c = 11;
+
+
+c = "Abo Omar";
+
+c = true;
+
+c = [5, 8, 10,5,8,5,8,5,8,5,8,5,8];
+
+//console.table(c);
+
+c = {
+    username: "adnan25",
+    password: "GyGreJJJ4656TTT!!",
+    ID: 20608
+}
+
+
+//console.table(c);
+
+
+let user = "Ahmad",
+  age = 23,
+    message = "Assalmualaikum";
+  
+let post;
+console.log(post);
+
+//let post = "21149 Hamburg"; // SyntaxError: Identifier 'post' has already been declared
+post = "21149 Hamburg";
+console.log(post);
+
+// console.log(message);
+// console.log(user);
+// console.log(age);
+
+/**
+ * Brackets [ ]
+ * Braces { }
+ * Parentheses ( )
+ */
