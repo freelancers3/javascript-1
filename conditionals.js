9ecf56548d5bd52a749c819d611e087932c030dd
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-01-07 20:20:55 +0100

    second commit

---
+++ conditionals.js
@@ -0,0 +1,32 @@
+let age = 18;
+
+if (age > 20) {
+  console.log("Enter Calssroom 1"); // age > 20 : false
+} else if (age < 6) {
+  console.log("Go to your mama"); // age < 6 : false
+} else if (age < 15) {
+  console.log("Enter Classroom x"); // age < 15 : false
+} else {
+  console.log("Enter Calssroom 2");
+}
+
+/**
+ * اكتب برنامج جافا سكريبت يستخدم عبارات if , else if , else 
+ * لتحديد هل الساعة الحالية صباحا "ص" أو مساءا "م" حسب الساعة الحالية
+ * استخدم نظام ٢٤ ساعة 
+ * 
+ * let hour = --- ;
+ * let amOrPm ;
+ * 
+ * if(condition){
+ * amOrPm = "AM";
+ * }elseif(condition){
+ * // your code 
+ * }else(condition){
+ * // your code
+ * }
+ * 
+ * console.log("It's currently " + amOrPm);
+ */
+
+
