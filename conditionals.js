6fa6a43eacb2fc48765754498ed3f271b2c65f5d
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-01-07 20:39:23 +0100

    init commit

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,5 @@
 let age = 18;
-
+/*
 if (age > 20) {
   console.log("Enter Calssroom 1"); // age > 20 : false
 } else if (age < 6) {
@@ -8,7 +8,7 @@ if (age > 20) {
   console.log("Enter Classroom x"); // age < 15 : false
 } else {
   console.log("Enter Calssroom 2");
-}
+}*/
 
 /**
  * اكتب برنامج جافا سكريبت يستخدم عبارات if , else if , else 
@@ -28,5 +28,30 @@ if (age > 20) {
  * 
  * console.log("It's currently " + amOrPm);
  */
+
+
+// prompt(message, default)
+
+// let name = prompt("Enetr your name: ", "Mohammed");
+// console.log("Hello " + name);
+
+// Get the hour 
+let hour = prompt("Enetr your current hour: ", 22);
+
+// declare amOrPm variable to hold "AM" or "PM":
+let amOrPm;
+
+if (hour >= 0 && hour < 12) {
+    amOrPm = "AM"
+} else if (hour >= 12 && hour < 24) {
+    amOrPm = "PM";
+} else { 
+    amOrPm = "invalid hour";
+}
+
+// print the result
+console.log("it's currently " + amOrPm);
+
+
 
 
